3339a9d4aaf6251539ec0f7c9254fcbde4d14b70
3DWARDRG <Edwardr185@gmail.com>
2023-06-24 22:04:33 -0500

    obtener valores para generar URL y hacer el llamado a la api

--- js/index.js
+++ js/index.js
@@ -8,3 +8,37 @@ fetch('')
 .catch(error => console.error(error));
 
 console.log("hola")
+
+
+const formulario= document.querySelector(".form-api");
+
+
+formulario.addEventListener("submit", obtenerValores)
+
+
+function obtenerValores (a){
+    a.preventDefault();
+    console.log("funciona")
+
+    let prueba= document.querySelectorAll(".form-control");
+
+    let valores=[];
+
+
+    for(i=0;i<prueba.length;i++){
+        
+        valores.push(prueba[i].value)
+
+        console.log(x)
+
+        if(x>=9 && x<=32){
+
+            console.log("agregariamos categorias a la url")
+        }
+
+    }
+
+
+
+
+}
