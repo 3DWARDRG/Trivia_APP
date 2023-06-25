391ed0d9147c52b977f34e570f1bc64f375ef09b
3DWARDRG <Edwardr185@gmail.com>
2023-06-24 23:59:07 -0500

    funcionamiento de prueba crea tu trivia 1.0

--- js/index.js
+++ js/index.js
@@ -1,12 +1,6 @@
 const UrlBase="https://opentdb.com/api.php?amount=10";
 
 
-
-fetch('')
-.then(Response => Response.json())
-.then(Response => console.log(Response))
-.catch(error => console.error(error));
-
 console.log("hola")
 
 
@@ -17,28 +11,51 @@ formulario.addEventListener("submit", obtenerValores)
 
 
 function obtenerValores (a){
+
     a.preventDefault();
-    console.log("funciona")
+
+    let newUrlConcatenada;
 
     let prueba= document.querySelectorAll(".form-control");
 
-    let valores=[];
-
-
     for(i=0;i<prueba.length;i++){
         
-        valores.push(prueba[i].value)
+       let x= prueba[i].value;
 
         console.log(x)
 
         if(x>=9 && x<=32){
+        
+            let argConcatenado="&category=";
 
-            console.log("agregariamos categorias a la url")
+            newUrlConcatenada= UrlBase+ argConcatenado + x;
+
+        }
+
+        else if(x==="easy" || x==="medium" || x==="hard"){
+
+            let argConcatenado="&difficulty=";
+
+            newUrlConcatenada+= argConcatenado + x;
+
+        }
+
+        else if(x==="multiple" || x==="boolean"){
+
+            let argConcatenado="&type=";
+
+            newUrlConcatenada+= argConcatenado + x;
+
         }
 
     }
 
+    newUrlConcatenada= String(newUrlConcatenada);
 
+    console.log(newUrlConcatenada)
 
+    sessionStorage.setItem('callUrlApi', newUrlConcatenada);
+
+    window.location.href="../TuTriviaPersonalizada.html";
 
 }
