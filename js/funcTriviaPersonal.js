2b7b94ba1d1ba35bb134804467f9aaf08df4ad9c
3DWARDRG <Edwardr185@gmail.com>
2023-06-27 01:57:58 -0500

    estructura 1.1 y funcionamiento automatizado 1.0 incompleto

--- js/funcTriviaPersonal.js
+++ js/funcTriviaPersonal.js
@@ -3,7 +3,44 @@ const callUrlApi= sessionStorage.getItem("callUrlApi");
 
 console.log(callUrlApi)
 
-fetch(callUrlApi)
-.then(Response => Response.json())
-.then(Response => console.log(Response))
-.catch(error => console.error(error));
+let answerPrueba;
+
+let elementDomPrueba= document.querySelectorAll("#prueba");
+
+console.log(elementDomPrueba)
+  
+  getData();
+
+
+  async function getData() {
+    try {
+      const response = await fetch(callUrlApi);
+      const data = await response.json();
+      console.log(data);
+
+      for(let i=0;i<elementDomPrueba.length;i++){
+        elementDomPrueba[i].innerHTML=[i+1]+"."+ " " +data.results[i].question
+
+        if(data.results[i].type==="boolean"){
+
+          console.log("funciona")
+        }
+
+        else if(data.results[i].type==="multiple"){
+          console.log("funciona")
+
+          let correctAnswer=data.results;
+          let incorrectAnswer;
+
+          let respuestasPrueba=correctAnswer + incorrectAnswer;
+          
+          for(let i=0;i<){}
+
+        }
+      }
+      // Código que se ejecutará después de que se cumpla la promesa
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
